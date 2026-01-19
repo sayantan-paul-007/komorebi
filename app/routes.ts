324f126e3f8cc141ae169ee726a9f0571e0f3607
Sayantan Paul <sayantanpaul222@gmail.com>
2026-01-19 22:03:36 +0530

    Completed Version 0

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 import path from "path";
 
-export default [index("routes/home.tsx"), route('/auth', "routes/auth.tsx")] satisfies RouteConfig;
+export default [index("routes/home.tsx"), 
+    route('/auth', "routes/auth.tsx"),
+    route('/upload', "routes/upload.tsx")] satisfies RouteConfig;
